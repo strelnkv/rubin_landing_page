f2dcd9c3629d0a0d047f06f70ebaf8c28c6fc388
Мария Стрельникова <mp.strelnikova@gmail.com>
2024-08-29 14:44:06 +0700

    layout with adaptiveness

---
+++ index.js
@@ -0,0 +1,7 @@
+// const hamburger = document.querySelector(".header__hamburger");
+// const nav = document.querySelector(".nav");
+
+// hamburger.addEventListener("click", () => {
+//   hamburger.classList.toggle("active");
+//   nav.classList.toggle("active");
+// });
